ebad8ed67514107c03f936176728a8236bd6ac2c
Yannicked <yannickdejong@zebry.nl>
2014-05-06 12:16:59 +0200

    Updated Userscript

--- userscript/pricer.user.js
+++ userscript/pricer.user.js
@@ -1,23 +1,12 @@
 // ==UserScript==
-// @name		TF2 Outpost pricer
-// @version		1.1
-// @description		Show prices of tf2 items on tf2outpost.com
+// @name		TF2 Outpost Bazaar Additions
+// @version		1.6.0
+// @description		Enrich your experience on bazaar and tf2outpost
 // @author		Yannick
 // @include		http://www.tf2outpost.com*
 // @include		http://bazaar.tf*
+// @icon                http://zebry.nl:8020?get=icon
+// @namespace           userscripts_tf2outpostbazaaradditions
 // @run-at document-end
 // ==/UserScript==
-
-
-
-$.getScript('http://95.47.140.204:8020?script=socketIO', function(data, textStatus, jqxhr){
-    console.log(jqxhr.status);
-	if (window.location.hostname.indexOf('bazaar.tf')> -1) {
-		$.getScript('http://95.47.140.204:8020?script=bazaar',function() {
-            
-		});
-	}else{
-		$.getScript('http://95.47.140.204:8020?script=outpost', function(){
-		});
-	}
-});
+location.href = 'javascript:$.getScript(\'http://zebry.nl:8020?script=socketIO\', function(data, textStatus, jqxhr){ if (window.location.hostname.indexOf(\'bazaar.tf\')> -1) { $.getScript(\'http://zebry.nl:8020?script=bazaar\',function() { console.debugtf2(\'loaded\'); }); }else{ $.getScript(\'http://zebry.nl:8020?script=outpost\', function(){ console.debugtf2(\'loaded\'); }); } });';
